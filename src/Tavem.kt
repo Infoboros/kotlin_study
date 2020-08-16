38aa2a50587acd2585c1930196e806b278bf6a03
[       ] <pintastd@gmail.com>
2020-08-16 21:00:53 +0300

    Глава 10

--- src/Tavem.kt
+++ src/Tavem.kt
@@ -1,3 +1,4 @@
+import java.io.File
 import kotlin.math.roundToInt
 
 const val TAVERN_NAME = "Taernyl's Folly"
@@ -5,13 +6,70 @@ const val TAVERN_NAME = "Taernyl's Folly"
 var playerGold = 10
 var playerSilver = 10
 
-fun main(args: Array<String>) {
+val patronList = mutableListOf("Eli", "Mordoc", "Sophie")
+val lastName = listOf("Ironfoot", "Fernsworth", "Baggins")
+val uniquePatrons = mutableSetOf<String>()
 
-    placeOrder("shandy,Dragon's Breath,5.2")
-    placeOrder("shandy,Dragon's Breath,5.2")
+val helloString = "*** Welcome to Taernyl's Folly ***"
+val menuList = File("data/tavern-menu-data.txt")
+        .readText()
+        .split('\n')
+        .sorted()
+
+private fun addDot(first: String, last: String, size: Int): String{
+    var dotString : String = first
+    (0..size - first.length - last.length).forEach { dotString += '.' }
+    return dotString + last
 }
 
-private fun performPurchase(price: Double): Boolean{
+fun main(args: Array<String>) {
+    if ("Eli" in patronList)
+        println("The tavern master says: Eli's in the back playing cards. ")
+    else
+        println("The tavern master says: Eli isn't here.")
+
+    if (patronList.containsAll(listOf("Sophie", "Mordoc")))
+        println("The tavern master says: Yea, they're seated by the stew kettle.")
+    else
+        println("The tavern master says: Nay, they departed hours ago.")
+
+
+    printMenu()
+
+    (0..9).forEach {
+        val first = patronList.shuffled().first()
+        val last = lastName.shuffled().first()
+        val name = "$first $last"
+        uniquePatrons += name
+    }
+
+    println(uniquePatrons)
+
+    var orderCount = 0
+    while (orderCount <= 9) {
+        placeOrder(uniquePatrons.shuffled().first(),
+                menuList.shuffled().first())
+        println()
+        orderCount++
+    }
+
+}
+
+private fun printMenu() {
+    println(helloString)
+    var oldType = ""
+    menuList.forEach {
+        val (type, name, price) = it.split(',')
+        if (type != oldType) {
+            oldType = type
+            println("           ~[$type]~")
+        }
+        println(addDot(name, price, helloString.length))
+    }
+    println()
+}
+
+private fun performPurchase(price: Double): Boolean {
     displayBalance()
     val totalPurse = playerGold + (playerSilver / 100.0)
     println("Total purse: $totalPurse")
@@ -32,30 +90,30 @@ private fun performPurchase(price: Double): Boolean{
     return true
 }
 
-private fun displayBalance(){
+private fun displayBalance() {
     println("Player's purse balance: Gold: $playerGold, Silver: $playerSilver")
 }
 
-private fun placeOrder(menuData: String){
+private fun placeOrder(patronName: String, menuData: String) {
     val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
     val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
-    println("Madrigal speaks with $tavernMaster about their order.")
+    println("$patronName speaks with $tavernMaster about their order.")
 
     val (type, name, price) = menuData.split(',')
 
-    if (performPurchase(price.toDouble())) {
-        val message = "Madrigal buys a $name ($type) for $price."
-        println(message)
-        val phrase = if (name == "Dragon's Breath") {
-            "Madrigal exclaims: ${toDragonSpeak("AH, DELICIOUS $name!")}"
-        }else{
-            "Madrigal says: Thanks for the $name!."
-        }
-        println(phrase)
-    }else{
-        val message = "Madrigal can't buys a $name ($type) for $price."
-        println(message)
+//    if (performPurchase(price.toDouble())) {
+    val message = "$patronName buys a $name ($type) for $price."
+    println(message)
+    val phrase = if (name == "Dragon's Breath") {
+        "$patronName exclaims: ${toDragonSpeak("AH, DELICIOUS $name!")}"
+    } else {
+        "$patronName says: Thanks for the $name!."
     }
+    println(phrase)
+//    } else {
+//        val message = "Madrigal can't buys a $name ($type) for $price."
+//        println(message)
+//    }
 }
 
 private fun toDragonSpeak(phrase: String) =
