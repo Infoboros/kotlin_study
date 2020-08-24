7fa47b88dd8b3308215837871219099b12076c58
[       ] <pintastd@gmail.com>
2020-08-24 22:12:32 +0300

    Глава 14

--- src/com/bignerdranch/nyethack/Game.kt
+++ src/com/bignerdranch/nyethack/Game.kt
@@ -14,6 +14,10 @@ fun main(args: Array<String>) {
     val player = Player("Madrigal")
     player.castFireball()
 
+    var currentRoom : Room = TownSquare()
+    println(currentRoom.description)
+    println(currentRoom.load())
+
     printPlayerStatus(player)
 }
 
